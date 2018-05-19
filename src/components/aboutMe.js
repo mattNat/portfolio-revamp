1a3859d99972e498c0a7c641985114637218814f
mattNat <mattsanpedro76@gmail.com>
2018-05-18 22:12:09 -0700

    add background images of outdoors and about me written

--- src/components/aboutMe.js
+++ src/components/aboutMe.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import '../styles/aboutMe.css';
+import ProfilePic from '../img/profilePic.JPG';
 
 export default function AboutMe() {
   return (
@@ -11,21 +12,15 @@ export default function AboutMe() {
               <div className="row">
                 <div className="col-lg-5 col-sm-6">
                   <div className="clearfix" />
-                  <h2 className="section-heading">Infinito Web Design Studio.</h2>
+                  <h2 className="section-heading">Thinkful Engineering Immersion Graduate</h2>
                   <br />
-                  <p className="lead">I'm a <a className="link" target="_blank" href="https://www.freecodecamp.com/thiagoferreira">self taught</a> web designer, developer, co-founder and entrepreneur based in Finland.<br />
-                    Hi!!!  I'm currently part of a small VR(Virtual Reality) development team in an upcoming <a className="link" target="_blank" href="https://www.facebook.com/Virtual-Dawn-802163799892155/">start-up called Virtual-Dawn</a>, developing awesome VR games.
-                    I'm also responsible for all of my company's web development needs.<br />
-                    My passion is to use technology based solutions, to help solve real world challenges.<br />
-                    Competences:<br />
-                    Languages and Frameworks:<br />
-                    Javascript, C#m HTML5, CSS3, jQuery, Bootstrap3, Angular.js, Meteor.js.<br />
-                    Tools &amp; expertise:<br />
-                    Git, Responsive Web Design, Agile Methodologies, Unity5 3D, Photoshop, VR Level Design, and Unity Generalist.
+                  <p className="lead">
+                    My passion for building things started my coding journey.  From setting up my own Linux server to learning how to deploy apps from a MERN stack, I enjoy tinkering with open-source software and discovering new things.<br/><br/>
+                    When I am not debugging, I enjoy trail running and hiking.  Some of the parks I've visited include Grand Canyon, Yosemite, Bryce Canyon, Death Valley, Monument Valley, Zion, Haleakala, and Joshua Tree.
                   </p>
                 </div>
                 <div className="col-lg-5 col-lg-offset-2 col-sm-6 collapse navbar-collapse">
-                  <img className="img-circle img-responsive" src="https://68.media.tumblr.com/ccd0e1b1bdb3492eecdac39b8d42d7ad/tumblr_nwh4pmSv8k1ud7rr3o1_400.jpg" alt />
+                  <img className="img-circle img-responsive" src={ProfilePic} alt />
                 </div>
               </div>
             </div>
