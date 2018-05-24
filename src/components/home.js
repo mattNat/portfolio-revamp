68109bde75dbd8e4fdf9e50cd687c4b985b579de
mattNat <mattsanpedro76@gmail.com>
2018-05-24 14:59:59 -0700

    rehaul nav bar and home section

--- src/components/home.js
+++ src/components/home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import '../styles/home.css';
+import OahuBeach from '../img/20170830_095819.jpg';
 
 export default function Home() {
   return (
@@ -11,30 +12,12 @@ export default function Home() {
               <div className="row">
                 <div className="col-lg-12">
                   <div className="intro-message">
-                    <h1>Matt San Pedro</h1>
-                    <h3>Full Stack Web Developer</h3>
+                    {/* <span> */}
+                      <h1>Matt San Pedro</h1>
+                      <h3>Full Stack Web Developer</h3>
+                    {/* </span> */}
                     <hr className="intro-divider" />
-                    <ul className="list-inline intro-social-buttons">
-                      {/* <li>
-                        <a href="https://twitter.com/Ferreir4Thiago" target="_blank" className="btn btn-default btn-lg"><i className="fa fa-twitter fa-fw" /> <span className="network-name">Twitter</span></a>
-                      </li> */}
-                      <li>
-                        <a href="mailto:mattsanpedro76@gmail.com" className="btn btn-default btn-lg">
-                        <i className="fa fa-envelope-o fa-fw" /> <span className="network-name">Email</span></a>
-                      </li>
-                      <li>
-                        <a href="https://github.com/mattNat" target="_blank" className="btn btn-default btn-lg ">
-                        <i className="fa fa-github fa-fw" /> <span className="network-name">Github</span></a>
-                      </li>
-                      <li>
-                        <a href="https://www.linkedin.com/in/matthew-san-pedro-m-s-34585623/" target="_blank" 
-                          className="btn btn-default btn-lg">
-                        <i className="fa fa-linkedin fa-fw" /> <span className="network-name">Linkedin</span></a>
-                      </li>
-                      {/* <li>
-                        <a href="https://www.freecodecamp.com/thiagoferreira" target="_blank" className="btn btn-default btn-lg"><i className="fa fa-free-code-camp fa-fw" /> <span className="network-name">freeCodeCamp</span></a>
-                      </li> */}
-                    </ul>
+                    <img src={OahuBeach} alt="Oahu beach"/>
                   </div>
                 </div>
               </div>
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,39 +4,21 @@ import '../styles/navbar.css';
 export default function NavBar() {
   return (
     <div className="NavBar">
-      <nav className="navbar navbar-default navbar-fixed-top topnav" role="navigation">
-        <div className="container topnav">
-          {/* Brand and toggle get grouped for better mobile display */}
-          <div className="navbar-header">
-            <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-              <span className="sr-only">Toggle navigation</span>
-              <span className="icon-bar" />
-              <span className="icon-bar" />
-              <span className="icon-bar" />
-            </button>
-            <a className="navbar-brand topnav" href="https://www.linkedin.com/in/matthew-san-pedro-m-s-34585623/" target="_blank">Matt San Pedro</a>
-          </div>
-          {/* Collect the nav links, forms, and other content for toggling */}
-          <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul className="nav navbar-nav navbar-right">
-              <li>
-                <a href="#home">Home</a>
-              </li>
-              <li>
-                <a href="#about">About</a>
-              </li>
-              <li>
-                <a href="#portfolio">Portfolio</a>
-              </li>
-              <li>
-                <a href="#contact">Contact</a>
-              </li>
-            </ul>
-          </div>
-          {/* /.navbar-collapse */}
-        </div>
-        {/* /.container */}
-      </nav>
+      <header>
+				<div class="navWrapper" id="home">
+					<div class=" clearfix">
+						{/* <h2 class="companyName"></h2> */}
+						<nav class="mainNav clearfix">
+							<ul>
+								<li><a href="#home">Home</a></li>
+								<li><a href="#about" class="smoothScroll">About</a></li>
+								<li><a href="#portfolio" class="smoothScroll">Portfolio</a></li>
+								<li><a href="#contact" class="smoothScroll">Contact</a></li>
+							</ul>
+						</nav>
+					</div>
+				</div>
+			</header>
     </div>
   );
 }
