3c1a4b1d7a7972582fd207d1d528f2819f1267fd
mattNat <mattsanpedro76@gmail.com>
2018-05-28 22:45:09 -0700

    update links to hiking friend github due to name change

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -30,9 +30,9 @@ export default function Portfolio() {
       screenshot: require('../img/hikingFriend.png'),
       title: "Hiking Friend",
       comment: "For those seeking adventure outdoors.  Combines both the Google Geolocations API and Hiking Project API to search hiking trails within a 10 mile radius.  Important trail data includes conditions, round-trip distance, and elevation.",
-      client: "https://github.com/mattNat/hiking-app/tree/master/client",
+      client: "https://github.com/mattsanpedro7/hiking-app/tree/master/client",
       // server: "https://github.com/mattNat/hiking-app/tree/master/server",
-      server: "https://github.com/mattNat/nestle-server",
+      server: "https://github.com/mattsanpedro7/nestle-server",
     },
   ]
 
