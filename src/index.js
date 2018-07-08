7de0604206d06f1b28c07d1b5e6baf4abc48d45e
mattsanpedro7 <mattsanpedro76@gmail.com>
2018-07-08 14:07:55 -0700

    add react-dom-router for blog link

--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
 import './index.css';
 import App from './App';
+import Blog from './components/blog';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
+
+ReactDOM.render(
+    // <Provider store={createStoreWithMiddleware(
+    //     reducers, 
+    //     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())}
+    //   >
+      <BrowserRouter>
+        <div>
+          <Switch>
+              <Route path='/blog' component={Blog} />
+              <Route path='/' component={App} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    // </Provider>
+  , document.getElementById('root'));
+  registerServiceWorker();
