69da92d8021465f0bd5619ae0a424ca12957ed90
mattsanpedro7 <mattsanpedro76@gmail.com>
2018-07-08 18:44:45 -0700

    start blog header styling

--- src/components/blog.js
+++ src/components/blog.js
@@ -5,6 +5,7 @@ import React from 'react';
 import axios from 'axios';
 
 import NavBar from './navbar';
+import '../styles/blog.css';
 
 export default class Blog extends React.Component {
     state = {
@@ -13,17 +14,27 @@ export default class Blog extends React.Component {
 
     componentDidMount() {
         axios.get(`http://public-api.wordpress.com/rest/v1/sites/mattdottech.wordpress.com/posts`)
-          .then(res => {
+            .then(res => {
             const blogs = res.data;
+            console.log(blogs);
             this.setState({ blogs });
-          })
+            })
     }
 
     render() {
         console.log(this.state.blogs.posts);
         return (
             <div className="blog">
+                <header className="intro-header">
                 <NavBar />
+                <div className="container">
+                    {/* <div className="intro-message"> */}
+                        <h1>Tech Blog</h1>
+                        {/* <h3>Full Stack Web Developer</h3> */}
+                        {/* <hr className="intro-divider" /> */}
+                    {/* </div> */}
+                </div>
+                </header>
             </div>
         )
     }
